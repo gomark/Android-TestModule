d389696fa65ed40fdb62a15268e07896f30b7883
Putti <putti.tang@gmail.com>
2020-09-05 16:50:06 +0700

    Initial commit

---
+++ utils/src/main/java/org/noxnox/utils/TestLib1.java
@@ -0,0 +1,46 @@
+package org.noxnox.utils;
+
+public class TestLib1 {
+
+    private int value1;
+    private int value2;
+    private int value3;
+    private int value4;
+    private int value5;
+
+    public TestLib1() {
+        super();
+    }
+
+    public int getValue1() {
+        return value1;
+    }
+
+    public void setValue1(int value1) {
+        this.value1 = value1;
+    }
+
+    public int getValue2() {
+        return value2;
+    }
+
+    public void setValue2(int value2) {
+        this.value2 = value2;
+    }
+
+    public int getValue3() {
+        return value3;
+    }
+
+    public void setValue3(int value3) {
+        this.value3 = value3;
+    }
+
+    public int getValue4() {
+        return value4;
+    }
+
+    public void setValue4(int value4) {
+        this.value4 = value4;
+    }
+}
