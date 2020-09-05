3573cbfa06520a89d16fb95078443b0908a35b50
Putti <putti.tang@gmail.com>
2020-09-06 06:45:12 +0700

    Ready to use

--- utils/src/main/java/org/noxnox/utils/TestLib2.java
+++ utils/src/main/java/org/noxnox/utils/TestLib2.java
@@ -11,5 +11,13 @@ public class TestLib2 {
     public void setValue1(int value1) {
         this.value1 = value1;
     }
+
+    public int getValue2() {
+        return value2;
+    }
+
+    public void setValue2(int value2) {
+        this.value2 = value2;
+    }
 }
 
